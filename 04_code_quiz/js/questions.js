5edbc138269003f442dbed5d71056c7a361f0ef7
JoannaChandler <jhazimih@gmail.com>
2020-12-15 18:46:35 -0500

    Commenting out JS for now

--- 04_code_quiz/js/questions.js
+++ 04_code_quiz/js/questions.js
@@ -1,4 +1,110 @@
-//TODO:
-// Create an object to hold the questions and answers
-// Might be an array of object?
-// Might be useful to be able to use the array indexes
+// //TODO:
+// // Create an object to hold the questions and answers
+// // Might be an array of objects?
+// // Might be useful to be able to use the array indexes
+
+// var questions = [
+//     {
+//         'question': 'Question 1 text',
+//         'answer1': 'Answer Option 1',
+//         'answer2': 'Answer Option 2',
+//         'answer3': 'Answer Option 3'
+//     },
+//     {
+//         'question': 'Question 2 text',
+//         'answer1': 'Answer Option 1',
+//         'answer2': 'Answer Option 2',
+//         'answer3': 'Answer Option 3'
+//     },
+//     {
+//        'question': 'Question 3 text',
+//         'answer1': 'Answer Option 1',
+//         'answer2': 'Answer Option 2',
+//         'answer3': 'Answer Option 3'
+//     },
+//     {
+//        'question': 'Question 4 text',
+//         'answer1': 'Answer Option 1',
+//         'answer2': 'Answer Option 2',
+//         'answer3': 'Answer Option 3'
+//     },
+//     {
+//         'question': 'Question 5 text',
+//         'answer1': 'Answer Option 1',
+//         'answer2': 'Answer Option 2',
+//         'answer3': 'Answer Option 3'
+//     }
+// ];
+
+// var questionsSlide = document.querySelector('.questions');
+// var questionsNum = document.getElementById('question-number');
+// var questionsText = document.getElementById('question-text');
+// var nextBtn = document.getElementById('next');
+// var numOf = 1;
+
+
+
+// questionsNum.textContent = numOf + ': ';
+// questionsText.textContent = questions[0].question;
+// var newText = questionsText.textContent = questions[0 + 1].question;
+
+// newText
+
+// var setText = function(){
+//   questionsText.textContent = questions[0 + 1].q
+// }
+
+// nextBtn.addEventListener('click', function(){
+//   setText();
+// })
+
+
+// var questions = [
+//     {
+//         'question': 'Question 1 text',
+//         'answer1': 'Answer Option 1',
+//         'answer2': 'Answer Option 2',
+//         'answer3': 'Answer Option 3'
+//     },
+//     {
+//         'question': 'Question 2 text',
+//         'answer1': 'Answer Option 1',
+//         'answer2': 'Answer Option 2',
+//         'answer3': 'Answer Option 3'
+//     },
+//     {
+//        'question': 'Question 3 text',
+//         'answer1': 'Answer Option 1',
+//         'answer2': 'Answer Option 2',
+//         'answer3': 'Answer Option 3'
+//     },
+//     {
+//        'question': 'Question 4 text',
+//         'answer1': 'Answer Option 1',
+//         'answer2': 'Answer Option 2',
+//         'answer3': 'Answer Option 3'
+//     },
+//     {
+//         'question': 'Question 5 text',
+//         'answer1': 'Answer Option 1',
+//         'answer2': 'Answer Option 2',
+//         'answer3': 'Answer Option 3'
+//     }
+// ];
+
+// var questionsSlide = document.querySelector('.questions');
+// var questionsNum = document.getElementById('question-number');
+// var questionsText = document.getElementById('question-text');
+// var nextBtn = document.getElementById('next');
+// var numOf = 0;
+
+// // var question = questionsText.textContent = questions[0].question;
+
+// // var setText = function(){
+// //   questionsText.textContent = questions[0 + 1].question;
+// //   questionsNum.textContent = numOf + 1 + ': ';
+// // }
+
+// nextBtn.addEventListener('click', function(){
+//    questionsText.textContent = questions[numOf++].question;
+// })
