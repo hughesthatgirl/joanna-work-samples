7d7e1937f8d57d2cefc6de09cc527cac1190c2b3
JoannaChandler <jhazimih@gmail.com>
2020-12-14 22:29:42 -0500

    Adding JS files with todos

---
+++ 04_code_quiz/js/logic.js
@@ -0,0 +1,4 @@
+//TODO:
+// STEP ONE:
+// Let's focus on getting the questions to pull in and the slides to show/hide
+// correctly first
---
+++ 04_code_quiz/js/questions.js
@@ -0,0 +1,4 @@
+//TODO:
+// Create an object to hold the questions and answers
+// Might be an array of object?
+// Might be useful to be able to use the array indexes
