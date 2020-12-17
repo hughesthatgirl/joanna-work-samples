1bb3c732c7ac7174bbabf37f506e5bb7c0552cb6
JoannaChandler <jhazimih@gmail.com>
2020-12-16 21:12:13 -0500

    Adding back feedback div and adding function to build the questions

--- 04_code_quiz/js/logic.js
+++ 04_code_quiz/js/logic.js
@@ -1,4 +1,4 @@
-var questionsObj = [
+var questionsArr = [
     {
         'question': 'Question 1 text',
         'answer1': 'Answer Option 1',
@@ -34,48 +34,64 @@ var questionsObj = [
 var timer = document.querySelector('#time');
 var startScreen = document.querySelector('#start-screen');
 var questionScreen = document.querySelector('#questions');
+var questionsContainer = document.querySelector('#questionsContainer');
 var resultScreen = document.querySelector('#end-screen');
 var leaderBoard = document.querySelector('#leaderboard');
 
 var currentScreen = 0;
 
-//Function to add text to question screens
-
-function buildQuestion(){
-	
+function buildQuestions(){
+	for (var i = 0; i < questionsArr.length; i++){
+		var screen = document.createElement('div');
+		screen.id = 'screen' + (i + 1);
+		screen.setAttribute('class', 'screen')
+		if (i === 0){
+			screen.classList.add('active');
+		}
+		
+		var title = document.createElement('h2');
+		title.setAttribute('class', 'section__heading');
+		title.textContent = (i + 1) + ': ' + questionsArr[i].question;
+		
+		
+		screen.appendChild(title);
+		questionsContainer.appendChild(screen);
+	}
 }
 
-//Function to show question screens
-function showScreen(num) {
-    screens[currentScreen].classList.remove('active');
-    screens[num].classList.add('active');
-    currentScreen = num;
+buildQuestions();
 
-    if(currentScreen === screens.length - 1){
-        nextBtn.style.opacity = '0';
-    }
-    else {
-        nextBtn.style.opacity = '1';
-    }
-}
+// //Function to show question screens
+// function showScreen(num) {
+//     screens[currentScreen].classList.remove('active');
+//     screens[num].classList.add('active');
+//     currentScreen = num;
 
-//Function to progress question screens
-function showNextScreen() {
-	showScreen(currentScreen + 1);
-}
+//     if(currentScreen === screens.length - 1){
+//         nextBtn.style.opacity = '0';
+//     }
+//     else {
+//         nextBtn.style.opacity = '1';
+//     }
+// }
 
-document.addEventListener('click', function(e){
-    if (e.target.id === 'start'){
-        startScreen.classList.add('hide');
-        questionScreen.classList.remove('hide');
-    }
+// //Function to progress question screens
+// function showNextScreen() {
+// 	showScreen(currentScreen + 1);
+// }
 
-    if (e.target.id === 'submit'){
-        resultScreen.classList.add('hide');
-        leaderBoard.classList.remove('hide');
-    }
+// document.addEventListener('click', function(e){
+//     if (e.target.id === 'start'){
+//         startScreen.classList.add('hide');
+//         questionScreen.classList.remove('hide');
+//     }
 
-    if (e.target.id === 'next'){
-        showNextScreen();
-    }
-})
+//     if (e.target.id === 'submit'){
+//         resultScreen.classList.add('hide');
+//         leaderBoard.classList.remove('hide');
+//     }
+
+//     if (e.target.id === 'next'){
+//         showNextScreen();
+//     }
+// })
