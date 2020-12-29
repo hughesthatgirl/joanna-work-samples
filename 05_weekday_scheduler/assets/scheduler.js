aad6ced12bb6ffd892b5893f327afbb19f03d504
JoannaChandler <jhazimih@gmail.com>
2020-12-29 16:45:32 -0500

    Changing vars to const

--- 05_weekday_scheduler/assets/scheduler.js
+++ 05_weekday_scheduler/assets/scheduler.js
@@ -6,14 +6,14 @@ dayjs.extend(window.dayjs_plugin_weekday);
 dayjs.extend(window.dayjs_plugin_weekYear);
 dayjs.extend(window.dayjs_plugin_weekOfYear);
 
-// currentDay.textContent = dayjs().format("MM-DD-YYYY");
+currentDay.textContent = dayjs().format("MM-DD-YYYY");
 
-var getDay = function(num){
+const getDay = function(num){
     return dayjs().day(num)
 }
 
 weekDays.forEach(function(day, index){
-    var item = document.createElement('li');
+    const item = document.createElement('li');
     item.textContent = getDay(index);
     dayList.appendChild(item);
 });
