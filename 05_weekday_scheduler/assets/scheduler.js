06f320faf0d7b5defc0f24d831bd948ed8516c96
JoannaChandler <jhazimih@gmail.com>
2020-12-29 16:44:10 -0500

    Outputting the days of the week into the #days list

--- 05_weekday_scheduler/assets/scheduler.js
+++ 05_weekday_scheduler/assets/scheduler.js
@@ -1,6 +1,19 @@
 const currentDay = document.querySelector('#currentDay');
-
-// currentDay.textContent = dayjs().format("MM-DD-YYYY");;
+const dayList = document.querySelector('#days');
+const weekDays = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 dayjs.extend(window.dayjs_plugin_weekday);
-currentDay.textContent = dayjs().weekday(2);
+dayjs.extend(window.dayjs_plugin_weekYear);
+dayjs.extend(window.dayjs_plugin_weekOfYear);
+
+// currentDay.textContent = dayjs().format("MM-DD-YYYY");
+
+var getDay = function(num){
+    return dayjs().day(num)
+}
+
+weekDays.forEach(function(day, index){
+    var item = document.createElement('li');
+    item.textContent = getDay(index);
+    dayList.appendChild(item);
+});
