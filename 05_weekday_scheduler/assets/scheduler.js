23670f6248528fdc8b3678a102b5ff60fea3a645
JoannaChandler <jhazimih@gmail.com>
2020-12-29 15:57:28 -0500

    Testing being able to display the weekdays

--- 05_weekday_scheduler/assets/scheduler.js
+++ 05_weekday_scheduler/assets/scheduler.js
@@ -1,3 +1,6 @@
 const currentDay = document.querySelector('#currentDay');
 
-currentDay.textContent = dayjs().format("MM-DD-YYYY");;
+// currentDay.textContent = dayjs().format("MM-DD-YYYY");;
+
+dayjs.extend(window.dayjs_plugin_weekday);
+currentDay.textContent = dayjs().weekday(2);
