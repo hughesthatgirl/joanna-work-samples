e5a45aabe19d869ef8dd580fe2f82803d9ed8958
JoannaChandler <jhazimih@gmail.com>
2020-12-30 11:40:34 -0500

    Looping through hours

--- 05_weekday_scheduler/assets/scheduler.js
+++ 05_weekday_scheduler/assets/scheduler.js
@@ -1,6 +1,6 @@
 const currentDay = document.querySelector('#currentDay');
-const dayList = document.querySelector('#days');
-const weekDays = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const hoursList = document.querySelector('#hours');
+const hoursArr = ['8','9', '10', '11', '12', '1', '2', '3', '4', '5']
 
 dayjs.extend(window.dayjs_plugin_weekday);
 dayjs.extend(window.dayjs_plugin_weekYear);
@@ -8,12 +8,14 @@ dayjs.extend(window.dayjs_plugin_weekOfYear);
 
 currentDay.textContent = dayjs().format("MM-DD-YYYY");
 
-const getDay = function(num){
-    return dayjs().day(num)
+const getHour = function(time){
+    return dayjs().hour(time);
 }
 
-weekDays.forEach(function(day, index){
+hoursArr.forEach(function(hour, index){
     const item = document.createElement('li');
-    item.textContent = getDay(index);
-    dayList.appendChild(item);
+    item.id = 'hour' + index;
+    item.setAttribute('class', 'hour__item');
+    item.textContent = getHour(index);
+    hoursList.appendChild(item);
 });
