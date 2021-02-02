94d5c1f4deae55a9f55fda5a71849dee54a5473c
JoannaChandler <jhazimih@gmail.com>
2021-02-02 13:53:38 -0500

    Testing adding br tags

--- 07_README_Generator/README.md
+++ 07_README_Generator/README.md
@@ -1,1 +1,41 @@
-# fadljglkfjglfk ## flgjkf
+
+        # Test Title</br>
+
+        [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)</br>
+
+        1. [Description](#description)
+        2. [Installation](#installation)
+        3. [Usage](#usage)
+        4. [Contribute](#contribute)
+        5. [Testing](#testing)
+        6. [Questions](#questions)
+        7. [Licensing](#licensing)</br>
+
+        ## Description</br>
+
+        Test Description</br>
+
+        ## Installation</br>
+
+        Fork the repo, clone to local, install dependencies, run node</br>
+
+        ## Usage</br>
+
+        See licensing agreement</br>
+
+        ## Contribute</br>
+
+        Via pull request</br>
+
+        ## Testing</br>
+
+        Before running node, be sure all dependencies are installed</br>
+
+        ## Questions?</br>
+
+        * test@gmail.com
+        * [Github Profile](https://github.com/testUser). /br>
+
+        ## Licensing</br>
+
+    
--- 07_README_Generator/index.js
+++ 07_README_Generator/index.js
@@ -13,43 +13,43 @@ const questions = [
         type: 'input',
         name: 'project_description',
         message: 'Description?'
+    },
+    {
+        type: 'input',
+        name: 'project_install',
+        message: 'List Installation Instructions:'
+    },
+    {
+        type: 'input',
+        name: 'project_usage',
+        message: 'Provide usage information:'
+    },
+    {
+        type: 'input',
+        name: 'project_contribution',
+        message: 'How can someone contribute?'
+    },
+    {
+        type: 'input',
+        name: 'project_tests',
+        message: 'Testing Guidelines:'
+    },
+    {
+        type: 'list',
+        name: 'badge',
+        message: 'Choose a license for this project:',
+        choices: ['MIT', 'ISC', 'Zlib']
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your Email Address:'
+    },
+    {
+        type: 'input',
+        name: 'github_username',
+        message: 'Enter your GitHub Username:'
     }
-    // {
-    //     type: 'input',
-    //     name: 'project_install',
-    //     message: 'List Installation Instructions:'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'project_usage',
-    //     message: 'Provide usage information:'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'project_contribution',
-    //     message: 'How can someone contribute?'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'project_tests',
-    //     message: 'Testing Guidelines:'
-    // },
-    // {
-    //     type: 'list',
-    //     name: 'badge',
-    //     message: 'Choose a license for this project:',
-    //     choices: ['MIT', 'ISC', 'Zlib']
-    // }
-    // {
-    //     type: 'input',
-    //     name: 'email',
-    //     message: 'Enter your Email Address:'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'github_username',
-    //     message: 'Enter your GitHub Username:'
-    // }
 ]
 
 inquirer.prompt(questions).then((data) => {
@@ -57,19 +57,58 @@ inquirer.prompt(questions).then((data) => {
 
     const title = data.project_title;
     const description = data.project_description;
-    // const installation = data.project_install;
-    // const usage = data.project_usage;
-    // const contribute = data.project_contribution;
-    // const tests = data.project_tests;
-    // const badge = data.badge;
-    // const email = data.email;
-    // const github = data.github_username;
+    const installation = data.project_install;
+    const usage = data.project_usage;
+    const contribute = data.project_contribution;
+    const tests = data.project_tests;
+    const badge = data.badge;
+    const email = data.email;
+    const github = data.github_username;
     
     // let licensing;
 
 
-    const body = `# ${title} ## ${description}`;
-    
+    const body = `
+        # ${title}</br>
+
+        [![License: ${badge}](https://img.shields.io/badge/License-${badge}-blue.svg)](https://opensource.org/licenses/${badge})</br>
+
+        1. [Description](#description)
+        2. [Installation](#installation)
+        3. [Usage](#usage)
+        4. [Contribute](#contribute)
+        5. [Testing](#testing)
+        6. [Questions](#questions)
+        7. [Licensing](#licensing)</br>
+
+        ## Description</br>
+
+        ${description}</br>
+
+        ## Installation</br>
+
+        ${installation}</br>
+
+        ## Usage</br>
+
+        ${usage}</br>
+
+        ## Contribute</br>
+
+        ${contribute}</br>
+
+        ## Testing</br>
+
+        ${tests}</br>
+
+        ## Questions?</br>
+
+        * ${email}
+        * [Github Profile](https://github.com/${github}). /br>
+
+        ## Licensing</br>
+
+    `;
 
     fs.writeFile(filename, body, (err) =>
       err ? console.log(err) : console.log('README Generated!')
